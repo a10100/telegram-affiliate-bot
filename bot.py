c7b91c2fe6feb871cd394c6075f1260889b6104a
a10100 <andreygentil2014@gmail.com>
2025-11-17 20:52:55 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,27 @@
 from flask import Flask
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 app = Flask(__name__)
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🤖 Bot afiliado está funcionando! Envie /link seguido do produto.")
+
+async def link(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if len(context.args) == 0:
+        await update.message.reply_text("Use assim: /link https://shopee.com/... ")
+        return
+    
+    original = context.args[0]
+    aff_link = original + "?aff_id=SEU_CÓDIGO_AQUI"
+    await update.message.reply_text(f"Aqui está seu link afiliado:\n{aff_link}")
+
 @app.route("/")
 def home():
-    return "Bot afiliado funcionando!"
+    return "Bot rodando!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
